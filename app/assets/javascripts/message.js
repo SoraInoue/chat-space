e41115fd0e26877c7675f03608d70812d13294a8
SoraInoue <66369807+SoraInoue@users.noreply.github.com>
2020-07-09 18:23:24 +0900

    [WIP]自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -41,6 +41,49 @@ $(function(){
         };
       }
 
+      function buildHTML(message){
+        if ( message.image ) {
+            //data-idが反映されるようにしている
+            var html =
+             `<div class="chat-main__message-list__box" data-message-id=${message.id}>
+                <div class="chat-main__message-list__box__upper-info">
+                  <div class="chat-main__message-list__box__upper-info__talker">
+                    ${message.user_name}
+                  </div>
+                  <div class="chat-main__message-list__box__upper-info__date">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="chat-main__message-list__box__text">
+                  <p class="lower-message__content">
+                    ${message.content}
+                  </p>
+                </div>
+                <img src=${message.image} >
+              </div>`
+            return html;
+          } else {
+            //同様にdata-idが反映されるようにしている
+            var html =
+             `<div class="chat-main__message-list__box" data-message-id=${message.id}>
+                <div class="chat-main__message-list__box__upper-info">
+                  <div class="chat-main__message-list__box__upper-info__talker">
+                    ${message.user_name}
+                  </div>
+                  <div class="chat-main__message-list__box__upper-info__date">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="chat-main__message-list__box__text">
+                  <p class="lower-message__content">
+                    ${message.content}
+                  </p>
+                </div>
+              </div>`
+            return html;
+          };
+        }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -65,4 +108,29 @@ $(function(){
     });
   });
 
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__message-list').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
